ab24b0d1f3c4dd30c722e962662608a5e0fa1eb0
kathyychenn <97640717+kathyychenn@users.noreply.github.com>
2022-03-13 07:01:01 -0700

    update cse12narytree and tester
    wrote big fat chunk of code

--- CSE12NaryTree.java
+++ CSE12NaryTree.java
@@ -1,9 +1,15 @@
 /**
- * TODO: Add file header
- * Name:
- * ID:
- * Email:
+ * Name: Kathy Chen
+ * ID: A17030814
+ * Email: ktchen@ucsd.edu
+ * Sources used: 
+ * https://docs.oracle.com/javase/10/docs/api/java/util/Queue.html
+ * https://docs.oracle.com/javase/10/docs/api/java/util/PriorityQueue.html
+ * https://learn.zybooks.com/zybook/UCSDCSE12Winter2022/chapter/6/section/2?content_resource_id=56801376
+ * 
  * File description: 
+ * This is the CSE12NaryTree java file for the CSE 12 programming final. It 
+ * contains the generic CSE12NaryTree class with a protected class Node within.
  */
 
 import java.util.List;
@@ -13,7 +19,11 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
- * TODO: Add class header
+ * A CSE12NaryTree class holds references to the root, the size, and the max
+ * num of children allowed per node. It contains helper methods utilized within
+ * the core methods, methods to modify the tree as well as a method to return a
+ * sorted traversal of the tree. Additionally, it also has a protected class 
+ * Node within.
  */
 public class CSE12NaryTree<E extends Comparable<E>> {
     
@@ -102,25 +112,178 @@ public class CSE12NaryTree<E extends Comparable<E>> {
     }
 
     /**
-     * TODO: Add method header
+     * Add new node containing element to the Nary tree in level order
+     * @param element - data of new node to be added to Nary Tree
      */
     public void add(E element) {
-        //TODO
+        //throw exception if element is null
+        if(element == null){
+            throw new NullPointerException();
+        }
+        
+        //if tree empty, set root to new element
+        if(this.size == 0){
+            this.root = new Node(element);
+            this.size++;
+            return;
+        }
+
+        //create queue for traversal
+        Queue<Node> traversal = new LinkedList<Node>();
+        traversal.add(this.root);
+
+
+        //check every element of NaryTree until element found or none are equal
+        while(!traversal.isEmpty()){
+            Node curr = traversal.poll();
+            
+            //Add node where there is room in children level
+            if(curr.getNumChildren()< this.N){
+                curr.addChild(new Node(element));
+                continue;
+            }
+            //if element not found yet, check children of curr
+            else{
+                for(Node child: curr.getChildren()){
+                    traversal.add(child);
+                }
+            }
+        }
+        //update size var
+        this.size++;
     }
 
     /**
-     * TODO: Add method header
+     * Check whether element is in the Nary tree 
+     * @param element - element to be checked if in Nary tree
      */
     public boolean contains(E element) {
-        //TODO
+        //throw exception if element is null
+        if(element == null){
+            throw new NullPointerException();
+        }
+
+        //create queue for traversal
+        Queue<Node> traversal = new LinkedList<Node>();
+        traversal.add(this.root);
+
+        //if tree empty, return false
+        if(this.size == 0){
+            return false;
+        }
+
+        //check every element of NaryTree until element found or none are equal
+        while(!traversal.isEmpty()){
+            Node curr = traversal.poll();
+            if(curr.getData().equals(element)){
+                return true;
+            }
+            //if element not found yet, check children of curr
+            if(curr.getNumChildren()!=0){
+                for(Node child: curr.getChildren()){
+                    traversal.add(child);
+                }
+            }
+        }
         return false;
     }
 
     /**
-     * TODO: Add method header
+     * Return an ArrayList of all elements sorted in ascending order
+     * @return ArrayList of elements in ascending order
      */
     public ArrayList<E> sortTree(){
-        //TODO
-        return null;
+        //return empty ArrayList if tree is empty
+        ArrayList<E> ret = new ArrayList<E>();
+        if(this.size == 0){
+            return ret;
+        }
+        if(this.size == 1){
+            ret.add((E)this.root.getData());
+            return ret;
+        }
+
+        //create queue for traversal, and tree to be heapSorted
+        Queue<Node> traversal = new LinkedList<Node>();
+        Node curr = this.root;
+        traversal.add(curr);
+        PriorityQueue<E> heapTree = new PriorityQueue<E>();
+
+        //add all elements of NaryTree to tree to be Heap Sorted
+        while(!traversal.isEmpty()){
+            Node node = traversal.poll();
+            heapTree.add(node.getData());
+
+            if(curr.getNumChildren()!=0){
+                for(Node child: node.getChildren()){
+                    traversal.add(child);
+                }
+            }
+        }
+        
+        //add values from heapTree to arrayList
+        for(E values: heapTree){
+            ret.add(values);
+        }
+
+        //sort arrayList by ascending order then return
+        this.heapSort(ret, ret.size());
+        return ret;
     }
+
+    /**
+     * Private helper method to heapSort arrayList in ascending order
+     * @param nums - arrayList to be sorted in ascending order
+     * @param numsSize - size of arrayList to be sorted
+     */
+    private void heapSort(ArrayList<E> nums, int numsSize){
+        //percolate down through each level
+        for(int i = numsSize / 2 - 1; i >= 0; i--){
+            MinHeapPercolateDown(i, nums, numsSize);
+        }
+        //remove values from heap
+        for(int i = numsSize - 1; i > 0; i--){
+            E temp = nums.get(0);
+            nums.set(0, nums.get(i));
+            nums.set(i, temp);
+            MinHeapPercolateDown(0, nums, i);
+        }
+    }
+
+    /**
+     * Private helper method to percolate down values to maintain min-heap
+     * properties
+     * @param nodeIndex - index of element to be percolated down
+     * @param nums - arrayList of elements to be percolated through
+     * @param numsSize - size of arrayList to be percolated through
+     */
+    private void MinHeapPercolateDown(int nodeIndex, ArrayList<E> nums, int numsSize){
+        int childIndex = 2 * nodeIndex + 1;
+        E value = nums.get(nodeIndex);
+        
+        while (childIndex < numsSize) {
+            // Find the min among the node and all the node's children
+            E minValue = value;
+            int minIndex = -1;
+            for (int i = 0; i < 2 && i + childIndex < numsSize; i++) {
+                if (nums.get(i + childIndex).compareTo(minValue) > 0) {
+                    minValue = nums.get(i + childIndex);
+                    minIndex = i + childIndex;
+                }
+            }
+        
+            if (minValue == value) {
+                return;
+            }
+            // Swap values of nodeIndex with the min value
+            else {
+                E temp = nums.get(nodeIndex);
+                nums.set(nodeIndex, nums.get(minIndex));
+                nums.set(minIndex, temp);
+                nodeIndex = minIndex;
+                childIndex = 2 * nodeIndex + 1;
+            }
+        }
+    }
+    
 }
--- CSE12NaryTreeTester.java
+++ CSE12NaryTreeTester.java
@@ -1,9 +1,10 @@
 /**
- * TODO: Add file header
- * Name:
- * ID:
- * Email:
- * File description: 
+ * Name: Kathy Chen
+ * ID: A17030814
+ * Email: ktchen@ucsd.edu
+ * File description: This file contains all of the student-written tests. 
+ * It contains test files for the CSE12NaryTree class to ensure accuracy 
+ * (return correct values, correctly modify sets, exceptions, etc) 
  */
  
 import static org.junit.Assert.*;
@@ -11,31 +12,159 @@ import org.junit.*;
 import java.util.*;
 
 /**
- * TODO: Add class header
+ * This class contains self-written J-unit tests for the CSE12NaryTree class.
  */
 public class CSE12NaryTreeTester {
+    private CSE12NaryTree<Integer> fiveNaryTree;
+    private CSE12NaryTree<Integer> rootNaryTree;
+    private CSE12NaryTree<Integer> emptyNaryTree;
+	//private MyLinkedList<String> threeStringList;
+
+	/**
+	 * Standard Test Fixture. An empty list, a list with one entry (0) and
+	 * a list with several entries (0,1,2)
+	 */
+	@Before
+	public void setUp() {
+		emptyNaryTree = new CSE12NaryTree<Integer>(5);
+        emptyNaryTree.size = 0;
+
+        rootNaryTree = new CSE12NaryTree<Integer>(5);
+        rootNaryTree.root = this.rootNaryTree.new Node(9);
+        rootNaryTree.size = 1;
+
+        fiveNaryTree = new CSE12NaryTree<Integer>(5);
+        fiveNaryTree.root = this.fiveNaryTree.new Node(9);
+        fiveNaryTree.root.addChild(this.fiveNaryTree.new Node(3));
+        fiveNaryTree.root.addChild(this.fiveNaryTree.new Node(7));
+        fiveNaryTree.root.addChild(this.fiveNaryTree.new Node(1));
+        fiveNaryTree.root.addChild(this.fiveNaryTree.new Node(2));
+        fiveNaryTree.root.addChild(this.fiveNaryTree.new Node(8));
+        fiveNaryTree.size = 6;
+	}
+    
     /**
-     * TODO: Add test case description.
+     * Test add method on 5-ary tree with a root node and its 5 children nodes.
      */
     @Test
     public void testAdd(){
+        fiveNaryTree.add(29);
+        fiveNaryTree.add(30);
+
+        //manually access data of new node added
+        List<CSE12NaryTree<Integer>.Node> rootChildren = 
+            fiveNaryTree.root.getChildren();
+        CSE12NaryTree<Integer>.Node expected1 = 
+            rootChildren.get(0).getChildren().get(0);
+        CSE12NaryTree<Integer>.Node expected2 = 
+            rootChildren.get(0).getChildren().get(1);
+
+        assertEquals("29 is first child of first child of root", (Integer)29, 
+                    expected1.getData());
+        assertEquals("30 is second child of first child of root", (Integer)30, 
+                    expected2.getData());
+        assertEquals("size incremented", 8, fiveNaryTree.size);
 
     }
 
     /**
-     * TODO: Add test case description
+     * Test add method on empty 5-ary tree
+     */
+    @Test
+    public void testAdd2(){
+        emptyNaryTree.add(29);
+
+        //manually access data of new node added
+        CSE12NaryTree<Integer>.Node expected1 = emptyNaryTree.root;
+
+        assertEquals("29 is the root", (Integer)29, expected1.getData());
+        assertEquals("size incremented", 1, emptyNaryTree.size);
+    }
+
+    /**
+     * Test add method when element is null
+     */
+    @Test
+    public void testAdd3(){
+        try {
+            fiveNaryTree.add(null);
+            fail();
+        } catch (Exception e) {
+            //exception caught successfully
+        }
+    }
+
+    /**
+     * Tests the contains method on a 5-ary tree when element is not present.
      */
     @Test
     public void testContains(){
-        
+        assertFalse("element not contained in NaryTree", 
+                    fiveNaryTree.contains(12));
+        assertFalse("element not contained in NaryTree", 
+                    fiveNaryTree.contains(0));
     }
 
     /**
-     * TODO: Add test case description
+     * Tests the contains method on a 5-ary tree when element is present.
+     */
+    @Test
+    public void testContains2(){
+        assertTrue("element contained in NaryTree", fiveNaryTree.contains(9));
+        assertTrue("element contained in NaryTree", fiveNaryTree.contains(2));
+    }
+
+    /**
+     * Tests the contains method when element is null.
+     */
+    @Test
+    public void testContains3(){
+        try {
+            fiveNaryTree.contains(null);
+            fail();
+        } catch (Exception e) {
+            //exception caught successfully
+        }
+    }
+
+    /**
+     * Tests the contains method on an empty tree.
+     */
+    @Test
+    public void testContains4(){
+        assertFalse("element not contained in emptyNaryTree", 
+                    emptyNaryTree.contains(12));
+        assertFalse("element not contained in emptyNaryTree", 
+                    emptyNaryTree.contains(0));
+    }
+
+    /**
+     * Tests sortTree method on 5-ary tree with only the root node.
      */
     @Test
     public void testSortTree(){
-        
+        ArrayList<Integer> expected = new ArrayList<Integer>();
+        expected.add(9);
+
+        assertEquals("sortTree returns list w root element", expected, 
+                    rootNaryTree.sortTree());
+    }
+
+    /**
+     * Tests sortTree method on 5-ary tree with root node and its 5 children
+     */
+    @Test
+    public void testSortTree2(){
+        ArrayList<Integer> expected = new ArrayList<Integer>();
+        expected.add(1);
+        expected.add(2);
+        expected.add(3);
+        expected.add(7);
+        expected.add(8);
+        expected.add(9);
+
+        assertEquals("sortTree returns list elements sorted", expected, 
+                    fiveNaryTree.sortTree());
     }
 
     /**
